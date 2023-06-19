2be28f0b5a98882fb2cc9254a0ef7e197bb571c3
doom-white <doom.white@yaani.com>
2023-06-19 17:20:44 +0300

    ReactDraggable Settings

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Draggable from "react-draggable";
 
 const Note = ({ note }) => {
   const [noteVisible, setNoteVisible] = useState(false);
@@ -8,26 +9,26 @@ const Note = ({ note }) => {
   };
 
   return (
-    <>
+    <Draggable defaultPosition={{ x: note.position.x, y: note.position.y }}>
       <div
         className="note-main"
         style={{
           position: "absolute",
-          top: note.position.y,
-          left: note.position.x,
+          top: 0,
+          left: 0,
         }}
         key={note.number}
       >
         <span
           className="note-box-number"
           style={{ "--color": note.color, cursor: "pointer" }}
-          onClick={showNotes}
+          onDoubleClick={showNotes}
         >
           {note.number}
         </span>
         {noteVisible && <div className="note-desc">{note.description}</div>}
       </div>
-    </>
+    </Draggable>
   );
 };
 
