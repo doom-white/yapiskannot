f0950ed1ba753f26e32b4d23c7df6ad789db5d0f
doom-white <doom.white@yaani.com>
2023-06-19 10:06:25 +0300

    Comment Box Styling -1

--- src/components/NoteBox.js
+++ src/components/NoteBox.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMouse } from "../MainContext";
 
 const NoteBox = () => {
-  const { boxPosition } = useMouse();
+  const { boxPosition, setMode, notes } = useMouse();
   const commentTypes = [
     {
       name: "comment",
@@ -20,12 +20,23 @@ const NoteBox = () => {
       text: "Note",
     },
   ];
+  const [colors, setColors] = useState(commentTypes[0].color);
+
+  const changeColor = (color) => {
+    setColors(color);
+  };
 
   return (
     <div
       className="note-box"
       style={{ position: "absolute", top: boxPosition.y, left: boxPosition.x }}
+      onMouseEnter={() => setMode(false)}
+      onMouseLeave={() => setMode(true)}
     >
+      <span className="note-box-number" style={{ "--color": colors }}>
+        {notes.length + 1}
+      </span>
+      <span className="upArrow"></span>
       <ul>
         {commentTypes.map((ct) => (
           <div className="comment-container" key={ct.name}>
@@ -33,7 +44,12 @@ const NoteBox = () => {
               className="comment-color"
               style={{ "--color": ct.color }}
             ></div>
-            <li>{ct.text}</li>
+            <li
+              onClick={() => changeColor(ct.color)}
+              style={{ "--color": ct.color }}
+            >
+              {ct.text}
+            </li>
           </div>
         ))}
       </ul>
